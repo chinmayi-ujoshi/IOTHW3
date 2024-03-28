6b9d5d3f2836714459e336bb63d4be9dccf183c4
Chinmayi Upendra Joshi <79636352+chinmayi-ujoshi@users.noreply.github.com>
2024-03-28 00:09:40 -0400

    Added Comments

--- runner.py
+++ runner.py
@@ -1,37 +1,41 @@
-import paho.mqtt.publish as publish
-from virtual_iot_station import VirtualIoTStation 
+import paho.mqtt.publish as mqtt_publisher
+from virtual_iot_station import IOTStation
 import time
 import datetime
 
-# ThingSpeak Channel ID.
-MQTT_CHANNEL_ID = "2463857"
+# Configuration for the MQTT channel on ThingSpeak.
+CHANNEL_IDENTIFIER = "2463857"
+MQTT_SERVICE_HOST = "mqtt3.thingspeak.com"
 
-# Hostname of ThingSpeak MQTT broker.
-MQTT_HOST = "mqtt3.thingspeak.com"
+# Authentication details for the MQTT connection.
+DEVICE_CLIENT_ID = "HAUVBjMLECwaMRw2BzkqJw0"
+CONN_USERNAME = "HAUVBjMLECwaMRw2BzkqJw0"
+CONN_PASSWORD = "78vz5w5GisMcZcS8WjjOPnlY"
 
-# Your MQTT credentials for the device
-MQTT_CLIENT_ID = "HAUVBjMLECwaMRw2BzkqJw0"
-MQTT_USERNAME  = "HAUVBjMLECwaMRw2BzkqJw0"
-MQTT_PASSWORD  = "78vz5w5GisMcZcS8WjjOPnlY"
+# Connection method and port for MQTT.
+SOCKET_TRANSPORT = "websockets"
+SERVICE_PORT = 80
 
-TRANSPORT = "websockets"
-PORT = 80
+# MQTT topic string for data publication.
+PUBLISH_CHANNEL = f"channels/{CHANNEL_IDENTIFIER}/publish"
 
-# Create the TOPIC string.
-TOPIC = "channels/" + MQTT_CHANNEL_ID + "/publish"
+# Initialize the simulated sensor station.
+environment_station = IOTStation()
 
-iot_station = VirtualIoTStation()
+# Infinite loop to continuously send data.
+while True:
+    # Retrieve simulated sensor readings.
+    temp_reading, humidity_reading, co2_reading = environment_station.get_sensor_data()
 
-while (True):
-  temperature, humidity, co2 = iot_station.generate_sensor_values()
+    # Format the data payload for MQTT publishing.
+    mqtt_payload = f"field1={temp_reading}&field2={humidity_reading}&field3={co2_reading}"
 
-  # build the payload string.
-  payload = "field1=" + str(temperature) + "&field2=" + str(humidity) + "&field3=" + str(co2)
+    # Log the data with the current timestamp.
+    current_timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    print(f"{current_timestamp} - Sending Data: {mqtt_payload} to MQTT Server: {MQTT_SERVICE_HOST}, ClientID: {DEVICE_CLIENT_ID}")
 
-  # Add logs
-  print (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f') + " Writing Payload=", payload," to host=", MQTT_HOST, " clientID=", MQTT_CLIENT_ID)
+    # Send the data to the ThingSpeak channel via MQTT.
+    mqtt_publisher.single(PUBLISH_CHANNEL, mqtt_payload, hostname=MQTT_SERVICE_HOST, transport=SOCKET_TRANSPORT, port=SERVICE_PORT, client_id=DEVICE_CLIENT_ID, auth={'username':CONN_USERNAME, 'password':CONN_PASSWORD})
 
-  # attempt to publish this data to the TOPIC.
-  publish.single(TOPIC, payload, hostname=MQTT_HOST, transport=TRANSPORT, port=PORT, client_id=MQTT_CLIENT_ID, auth={'username':MQTT_USERNAME,'password':MQTT_PASSWORD})
-
-  time.sleep(5)
+    # Wait for 5 seconds before sending the next set of data.
+    time.sleep(5)
